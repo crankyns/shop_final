9fe4c65de28704d08278a270cd043b5b4c1794dc
Vladislav Urievich <vladik040599@gmail.com>
2021-12-08 15:19:30 +0300

    corrections

--- romanio/mainapp/models.py
+++ romanio/mainapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from user.models import CustomUser
+
 
 
 class Product(models.Model):
@@ -59,31 +59,3 @@ class SubCategory(models.Model):
     def get_absolute_url(self):
         return reverse('subcategory',  kwargs={"slug_subcategory":self.slug, "slug_category":self.category.slug})
 
-# class OrderProduct(models.Model):
-
-#     product = models.ForeignKey(Product, verbose_name="Товар к заказу", on_delete=models.CASCADE)
-#     quantity = models.PositiveSmallIntegerField(verbose_name="Количество товара")
-#     summary_price = models.DecimalField(max_digits=10, verbose_name="Итоговая стоимость продукта")
-
-#     def __str__(self):
-#         return self.product
-
-#     class Meta():
-#         verbose_name = "Товар к заказу"
-#         verbose_name_plural = "Товары к заказу"
-
-    
-# class Order(models.Model):
-
-#     products = models.ForeignKey(OrderProduct, verbose_name="Товары в заказе", on_delete=models.CASCADE)
-#     owner = models.OneToOneField(CustomUser, verbose_name="Владелец заказа", on_delete=models.CASCADE)
-#     city = models.CharField(max_length=30, verbose_name="Город доставки")
-#     adress = models.CharField(max_length=70, verbose_name="Адрес доставки")
-#     price = models.DecimalField(max_digits=10, verbose_name="Стоимость заказа")
-
-#     def __str_(self):
-#         return self.owner, self.products
-
-#     class Meta():
-#         verbose_name = "Заказ"
-#         verbose_name_plural = "Заказы"
